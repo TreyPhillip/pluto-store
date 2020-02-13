c34faea173163e0b5c5558ec0f6aaed91ee43363
Nathan Hannon <nathan@hannon.me>
2020-02-12 23:48:06 -0500

    Changed tags and added more properties to css

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,11 +7,14 @@ import { BrowserRouter } from 'react-router-dom';
 
 export default class App extends Component {
 	static displayName = App.name;
+	
 
-	//TODO: Solve Routing Problem in this file and NavMenu.
+	//TODO: Solve Routing Problem in this file and NavMenu. (FINISHED)
+
 	render () {
 		return (
 		<Layout>
+			{/* DO NOT DELETE THESE ROUTER TAGS */}
 			<BrowserRouter>
 				<div>
 					<Route exact path='/' component={Home} />
