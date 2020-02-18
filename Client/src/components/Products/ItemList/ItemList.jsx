e72ecb86e51646878b28c6d6c8b21b185ab86013
Nathan Hannon <nathan@hannon.me>
2020-02-17 19:13:44 -0500

    removed redundant folder

--- Client/src/components/Products/ItemList/ItemList.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import './ItemList.css';
-import { ItemCard } from '../ProductCard/ProductCard';
-
-//Creates the item list using the card component
-export const ItemList = props => (
-    <div className="item-list">{props.item.map((item) => <ItemCard key={item.id} item={item} />)} </div>
-);
