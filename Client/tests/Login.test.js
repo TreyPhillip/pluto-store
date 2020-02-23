0955241e459a35838852548a22a7c6efc6b277fe
Nathan Hannon <nathan@hannon.me>
2020-02-23 17:39:19 -0500

    renamed login test const

--- Client/tests/Login.test.js
+++ Client/tests/Login.test.js
@@ -4,6 +4,6 @@ import Login from '../src/components/Login/Login';
 
 test('renders login page', () => {
   const { getByText } = render(<Login />);
-  const linkElement = getByText(/Username/i);
-  expect(linkElement).toBeInTheDocument();
+  const formElement = getByText(/Username/i);
+  expect(formElement).toBeInTheDocument();
 });
