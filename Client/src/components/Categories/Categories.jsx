dbf61b51126733faadd2891cbe671a94b054497c
Nathan Hannon <nathan@hannon.me>
2020-02-24 18:09:56 -0500

    Implemented basic category page

--- Client/src/components/Categories/Categories.jsx
+++ Client/src/components/Categories/Categories.jsx
@@ -1,2 +1,24 @@
-import { React } from 'react';
+import React, { Component } from 'react';
 import './Categories.css';
+
+export  class Categories extends Component {
+    constructor() {
+        super();
+        this.state = {
+          categories: []
+        };
+    }
+     //pull data from the backend (database)
+    componentDidMount() {
+        fetch("http://localhost:5000/categories")
+        .then(res => res.json())
+        .then(data => this.setState({ categories: data }));
+    }
+    render() {
+        return (
+        <div className="category-method">
+            <p category={this.state.categories} />
+        </div>
+        );
+    }
+}
