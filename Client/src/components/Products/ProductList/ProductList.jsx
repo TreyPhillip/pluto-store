79158281cadec8cb1995d7535c74c247076cef3e
Nathan Hannon <nathan@hannon.me>
2020-03-11 22:33:27 -0400

    Added classes for card components

--- Client/src/components/Products/ProductList/ProductList.jsx
+++ Client/src/components/Products/ProductList/ProductList.jsx
@@ -12,10 +12,10 @@ export const ProductList = props => (
     <div className="product-list">{props.product.map((product) => 
         <Link to={"/Details/" + props.product.productid}>
         <div>
-            <Card key={product.id} product={product}>
+            <Card className = "card text-white bg-primary mb-3" key={product.id} product={product}>
                 <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-                <CardBody>
-                <CardTitle>{props.product.productname}</CardTitle> 
+                <CardBody className="card-body">
+                <CardTitle className="card-title">{props.product.productname}</CardTitle> 
                 </CardBody>
             </Card>
         </div>
