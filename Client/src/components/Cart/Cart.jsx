96bd12e83c73d9673d116eee30e7d094d98914ac
Trey <treyphillip@live.com>
2020-03-15 17:47:01 -0400

    Cart display functionality.

--- Client/src/components/Cart/Cart.jsx
+++ Client/src/components/Cart/Cart.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import './Cart.css';
-import { Container, Form, FormGroup, Label, Input, Button } from "reactstrap";
+import { Container, Form, FormGroup, Label, Input, Button } from 'reactstrap';
 
 export class Cart extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			cartItems: JSON.parse(sessionStorage.getItem("cart")) 
+			cartItems: JSON.parse(sessionStorage.getItem('cart')),
+			productDetails: []
+			//cartItems: []
 		};
-		
+	}
+	componentDidMount() {
+		testAddToCart(this.state.productDetails);
+		console.log(this.state.cartItems);
 	}
 
 	render() {
@@ -16,53 +21,62 @@ export class Cart extends Component {
 			<div>
 				<div class="shopping-cart">
 					<div class="column-labels">
-						<Label className="image">Image</Label>
-						<Label className="details">Product</Label>
+						<Label className="details">Item</Label>
 						<Label className="price">Price</Label>
-						<Label className="quantity">Quantity</Label>
 						<Label className="removal">Remove</Label>
 						<Label className="line-price">Total</Label>
 					</div>
 
-					<div class="product">
-						<div class="image">
-							<img src={this.state.itemImage} />
-						</div>
-						<div class="details">
-							<div class="title">{'test product'}</div>
-						</div>
-						<div class="price">{'500'}</div>
-						<div class="quantity">{'2'}</div>
-						<div class="removal">
-							<Button color="danger"
-								onClick={() => {
-									// removeItem(this.state.itemID);
-								}}
-								class="remove-product">
-								Remove
-							</Button>
-						</div>
-						<div class="line-price">{this.state.total}</div>
-					</div>
-
-					{/* {this.state.cartItems &&
+					{this.state.cartItems &&
 						this.state.cartItems.map((item) => (
-							<CartDetail
-								key={item.itemID}
-								itemID={item.itemID}
-								itemImage={item.itemImage}
-								itemName={item.itemName}
-								price={item.price}
-								quantity={item.quantity}
-								total={item.total}
-							/>
-						))} */}
+							<div class="product">
+								<div class="details">
+									<div class="title">{item.productName}</div>
+								</div>
+								<div class="price">{item.price}</div>
+								{/* <div class="quantity">{item}</div> */}
+								<div class="removal">
+									<Button color="danger" class="remove-product">
+										Remove
+									</Button>
+								</div>
+								<div class="line-price">{'111'}</div>
+							</div>
+						))}
 
 					{/* <Link to="/"> */}
-					<Button color="success" className="checkout">Checkout</Button>
+					<Button color="success" className="checkout">
+						Checkout
+					</Button>
 					{/* </Link> */}
 				</div>
 			</div>
 		);
 	}
+}
+//debug purposes only, will be implemented in product detail page
+function testAddToCart(productInfo) {
+	let cartItems = [];
+	var product = {
+		//productID: productInfo.productID
+		productID: 1,
+		productName: 'Razer Deathadder Elite',
+		catrgoryID: 1,
+		sellerID: 1,
+		price: 99.99,
+		description: 'Razer mouse, elite editon'
+	};
+	var product1 = {
+		//productID: productInfo.productID
+		productID: 1,
+		productName: 'Razer Deathadder',
+		catrgoryID: 1,
+		sellerID: 1,
+		price: 99.99,
+		description: 'Razer mouse, standard editon'
+	};
+
+	cartItems.push(product);
+	cartItems.push(product1);
+	sessionStorage.setItem('cart', JSON.stringify(cartItems));
 }
