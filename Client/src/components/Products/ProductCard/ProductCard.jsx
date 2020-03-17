23d29e8f4b706705a5ed4ec8b92db0b55800aa0c
Nathan Hannon <nathan@hannon.me>
2020-03-17 09:12:36 -0400

    Added  more code to details page

--- Client/src/components/Products/ProductCard/ProductCard.jsx
+++ Client/src/components/Products/ProductCard/ProductCard.jsx
@@ -8,7 +8,7 @@ import './ProductCard.css';
 
 export const ProductCard = props => {
     return (
-        <Link to={"/Details/" + props.product.productid}>
+        <Link to={props.product.productid + "/details/"}>
             <div>
                 <Card>
                     <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
