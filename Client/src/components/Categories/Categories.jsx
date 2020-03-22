37e209a313bde6c95e07d0e4a719469878b4942e
Nathan Hannon <nathan@hannon.me>
2020-03-22 14:26:40 -0400

    reduced code spacing

--- Client/src/components/Categories/Categories.jsx
+++ Client/src/components/Categories/Categories.jsx
@@ -17,24 +17,24 @@ export  class Categories extends Component {
     }
     render() {
         return (
-                <ListGroup flush>
-                    <ListGroupItem tag="a" href="/Appliances" action>Appliances</ListGroupItem>
-                    <ListGroupItem tag="a" href="/Automotive" action>Automotive</ListGroupItem>
-                    <ListGroupItem tag="a" href="/Books" action>Books</ListGroupItem>
-                    <ListGroupItem tag="a" href="/Cooking" action>Cooking</ListGroupItem>
-                    <ListGroupItem tag="a" href="/Electrionics"action>Electronics</ListGroupItem>
-                    <ListGroupItem tag="a" href="/Fashion" action>Fashion</ListGroupItem>
-                    <ListGroupItem tag="a" href="/Garden" action>Garden</ListGroupItem>
-                    <ListGroupItem tag="a" href="/Food" action>Grocery and Whole Foods</ListGroupItem>
-                    <ListGroupItem tag="a" href="/Home" action>Home</ListGroupItem>
-                    <ListGroupItem tag="a" href="/Entertainment" action>Movies and TV Shows</ListGroupItem>
-                    <ListGroupItem tag="a" href="/Music" action>Music</ListGroupItem>
-                    <ListGroupItem tag="a" href="/Office" action>Office</ListGroupItem>
-                    <ListGroupItem tag="a" href="/Outdoors" action>Outdoors</ListGroupItem>
-                    <ListGroupItem tag="a" href="/Software" action>Software and Video Games</ListGroupItem>
-                    <ListGroupItem tag="a" href="/Sports" action>Sports</ListGroupItem>
-                    <ListGroupItem tag="a" href="/Toys" action>Toys and Games</ListGroupItem>
-                </ListGroup>
+            <ListGroup flush>
+                <ListGroupItem tag="a" href="/Appliances" action>Appliances</ListGroupItem>
+                <ListGroupItem tag="a" href="/Automotive" action>Automotive</ListGroupItem>
+                <ListGroupItem tag="a" href="/Books" action>Books</ListGroupItem>
+                <ListGroupItem tag="a" href="/Cooking" action>Cooking</ListGroupItem>
+                <ListGroupItem tag="a" href="/Electrionics"action>Electronics</ListGroupItem>
+                <ListGroupItem tag="a" href="/Fashion" action>Fashion</ListGroupItem>
+                <ListGroupItem tag="a" href="/Garden" action>Garden</ListGroupItem>
+                <ListGroupItem tag="a" href="/Food" action>Grocery and Whole Foods</ListGroupItem>
+                <ListGroupItem tag="a" href="/Home" action>Home</ListGroupItem>
+                <ListGroupItem tag="a" href="/Entertainment" action>Movies and TV Shows</ListGroupItem>
+                <ListGroupItem tag="a" href="/Music" action>Music</ListGroupItem>
+                <ListGroupItem tag="a" href="/Office" action>Office</ListGroupItem>
+                <ListGroupItem tag="a" href="/Outdoors" action>Outdoors</ListGroupItem>
+                <ListGroupItem tag="a" href="/Software" action>Software and Video Games</ListGroupItem>
+                <ListGroupItem tag="a" href="/Sports" action>Sports</ListGroupItem>
+                <ListGroupItem tag="a" href="/Toys" action>Toys and Games</ListGroupItem>
+            </ListGroup>
         );
     }
 }
