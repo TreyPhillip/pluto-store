9951c7199c5107687bb2534d1fb64619dd4d3952
Nathan Hannon <nathan@hannon.me>
2020-03-29 00:32:03 -0400

    Started on wishlist

---
+++ Client/src/components/Wishlist/Wishlist.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { ProductDetails } from '../Products/ProductDetails/ProductDetails';
+
+export  class Wishlist extends Component {
+    constructor() {
+        super();
+        this.state = {
+          wishlist: []
+        };
+    }
+     //pull data from the backend (database)
+    componentDidMount() {
+        fetch("http://localhost:5000/Wishlist")
+        .then(res => res.json())
+        .then(data => this.setState({ categories: data }));
+    }
+    render() {
+        return (
+            <ListGroup flush>
+                <ListGroupItem>${ProductDetails.productname}</ListGroupItem>
+            </ListGroup>
+        );
+    }
+}
