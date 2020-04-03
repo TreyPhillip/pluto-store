6532e47be4478b8212d35fc3c1cdb9ec75a5be5b
Nathan Hannon <nathan@hannon.me>
2020-04-02 21:48:23 -0400

    Updated category pages

--- Client/src/components/Categories/Types/Appliances.jsx
+++ Client/src/components/Categories/Types/Appliances.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import ProductList from '../../Products/ProductList/ProductList'
+import Axios from 'axios';
+import { ProductList } from '../../Products/ProductList/ProductList'
+import {loadUser} from '../../Actions/authAction';
+import { connect } from 'react-redux';
 
 export class Appliances extends Component {
     constructor() {
@@ -10,7 +13,7 @@ export class Appliances extends Component {
       }
     //pull data from the backend (database)
     componentDidMount() {
-    var token = cookie.load("token");
+    //var token = cookie.load("token");
     fetch("http://localhost:5000/Appliances")
       .then(res => res.json())
       .then(data => this.setState({ appliances: data }));
--- Client/src/components/Categories/Types/Office.jsx
+++ Client/src/components/Categories/Types/Office.jsx
@@ -1,23 +1,42 @@
 import React, { Component } from 'react';
-import ProductList from '../../Products/ProductList/ProductList'
+import Axios from 'axios';
+import { ProductList } from '../../Products/ProductList/ProductList'
+import {loadUser} from '../../Actions/authAction';
+import { connect } from 'react-redux';
 
 export class Office extends Component {
-    constructor() {
-        super();
-        this.state = {
-          office: [],
-        };
-      }
-    //pull data from the backend (database)
-    componentDidMount() {
-    var token = cookie.load("token");
-    fetch("http://localhost:5000/Office")
-      .then(res => res.json())
-      .then(data => this.setState({ office: data }));
-    }
-    render() {
-        return (
-            <ProductList product={this.state.office} />
-        )
-    }
+  constructor() {
+    super();
+    this.state = {
+      office: [],
+    };
+  }
+//pull data from the backend (database)
+componentDidMount() {
+  const {user} = this.props.auth;
+  let id  = window.location.href.split('/')[4];
+
+//var token = cookie.load("token");
+this.props.loadUser();
+Axios.get("http://localhost:5000/getAllProductsByCategory/1")
+  .then(data => this.setState({ office: data}));
 }
+render() {
+    const {user} = this.props.auth;
+    console.log(user)
+    console.log(this.state.office)
+
+    return (
+      <div>
+         {/*<ProductList product={this.state.office} />*/}
+      </div>
+    )
+}
+}
+
+const mapStateToProps = state =>({
+auth: state.auth,
+error:state.error
+})
+
+export default connect(mapStateToProps, {loadUser})(Office);
